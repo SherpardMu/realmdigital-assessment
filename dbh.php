6e55a5b0582d547859899094cd2598a9c73331cf
SherpardMu <91271972+SherpardMu@users.noreply.github.com>
2021-09-23 16:45:33 +0200

    Add files via upload

---
+++ dbh.php
@@ -0,0 +1,14 @@
+<?php
+error_reporting(E_ALL & ~E_NOTICE);
+$servername = "localhost";
+$dBUsername = "infintech";
+$dbPassword = "tech@2021";
+$dBName = "ems";
+
+/*$conn = mysqli_connect($servername, $dBUsername, $dbPassword, $dBName);
+
+if(!$conn){
+	echo "Databese Connection Failed";
+}*/
+
+?>
